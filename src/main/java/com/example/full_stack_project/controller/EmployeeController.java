975f1acae1f2ee28d7f5447393f73021cf658452
shubhanginaik <shubhangi.naik83@gmail.com>
2025-05-16 13:39:55 +0300

    add cross origin to the controller

--- src/main/java/com/example/full_stack_project/controller/EmployeeController.java
+++ src/main/java/com/example/full_stack_project/controller/EmployeeController.java
@@ -4,14 +4,16 @@ import com.example.full_stack_project.model.Employee;
 import com.example.full_stack_project.service.EmployeeService;
 import com.example.full_stack_project.service.EmployeeServiceImpl;
 import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/v1/employee")
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
   private EmployeeService employeeService;
 
